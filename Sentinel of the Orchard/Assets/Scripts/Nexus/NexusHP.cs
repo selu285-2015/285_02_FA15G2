f1a0f841de3773b302890d5246f424c0ec0a8a07
Kane <michael.fontenot-2@selu.edu>
2015-09-19 09:35:37 -0500

    This is the Current working build.
    -Demo Map
    -1 Enemy

---
+++ Sentinel of the Orchard/Assets/Scripts/Nexus/NexusHP.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NexusHP : MonoBehaviour {
+
+	public int startingHP;
+	public int currentHP;
+	public Slider hitPoints;
+
+	bool isDead;
+	bool damage;
+
+
+	// Use this for initialization
+	void Start() {
+		currentHP = startingHP;
+	}
+
+	
+	
+	public void TakeDamage (int amount)
+	{
+		damage = true;
+
+		currentHP -= amount;
+		
+
+		hitPoints.value = currentHP;
+		if (currentHP <= 0 && !isDead) {
+			// ... it should die.
+			Death ();
+		}
+
+	}
+		// Update is called once per frame
+		void Update () {
+			if (damage)
+			{
+				//for later effects
+		}
+		damage = false;
+	}
+
+	void Death()
+	{
+		isDead = true;
+		//code for game over screen 
+	}
+}
