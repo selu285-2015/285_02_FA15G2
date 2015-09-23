d2daf8632698a5c4ddb47da8d7df97df3e7391f1
Kane <michael.fontenot-2@selu.edu>
2015-09-22 19:47:42 -0500

    Fixing the Garbage

--- Sentinel of the Orchard/Assets/Scripts/Towers/TowerBase.cs
+++ Sentinel of the Orchard/Assets/Scripts/Towers/TowerBase.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
-//using TreeEditor;
+using TreeEditor;
 
 public class TowerBase : MonoBehaviour
 {
@@ -29,7 +29,6 @@ public class TowerBase : MonoBehaviour
 		TowerList[towerCount] = self;
 		towerCount++;
 	}
-<<<<<<< HEAD
 	
 	private void StackPush(GameObject mob)
 	{
@@ -95,7 +94,7 @@ public class TowerBase : MonoBehaviour
 	void Fire(GameObject creep, int damage)
 	{
 		EnemyHealth trg = creep.GetComponent<EnemyHealth>();
-		trg.TakeDMG(damage, Vector3.back);
+		trg.TakeDMG(damage, Vector3.back, self);
 		
 		gunLine.enabled = true;
 		gunLine.SetPosition(0, transform.position);
@@ -105,71 +104,23 @@ public class TowerBase : MonoBehaviour
 		
 		if (trg.currentHP <= 0)
 		{
-			for (int i = 0; i < 50; i++)
-			{
-				if (TowerList[i] != null && TowerList[i].CurrentTarget == trg)
-				{
-					TowerList[i].CurrentTarget = null;
-				}
-			}
+			
 			CurrentTarget = null;
 			Destroy(creep);
 		}
 		
 	}
 	
+	public void ClearTargets(GameObject trg)
+	{
+		for (int i = 0; i < 50; i++)
+		{
+			if (TowerList[i] != null && TowerList[i].CurrentTarget == trg)
+			{
+				TowerList[i].CurrentTarget = null;
+			}
+		}
+	}
+	
 	
 }
-=======
-
-    void OnTriggerEnter(Collider mob)
-    {
-        if (mob.gameObject.CompareTag("Creep"))
-        {
-            StackPush(mob.gameObject);
-        }
-    }
-
-    void OnTriggerExit(Collider mob)
-    {
-        if (mob.gameObject.CompareTag("Creep") && CurrentTarget == mob.gameObject)
-        {
-            CurrentTarget = null;
-        }
-    }
-
-
-    void Fire(GameObject creep, int damage)
-    {
-        EnemyHealth trg = creep.GetComponent<EnemyHealth>();
-        trg.TakeDMG(damage, Vector3.back);
-
-        gunLine.enabled = true;
-        gunLine.SetPosition(0, transform.position);
-        shootRay.origin = transform.position;
-        shootRay.direction = transform.position - creep.transform.position;
-        gunLine.SetPosition(1, shootRay.origin - shootRay.direction * Vector3.Distance(CurrentTarget.transform.position, self.gameObject.transform.position) * 1f);
-        
-        if (trg.currentHP <= 0)
-        {
-            
-            CurrentTarget = null;
-            Destroy(creep);
-        }
-
-    }
-
-    public void ClearTargets(GameObject trg)
-    {
-        for (int i = 0; i < 50; i++)
-        {
-            if (TowerList[i] != null && TowerList[i].CurrentTarget == trg)
-            {
-                TowerList[i].CurrentTarget = null;
-            }
-        }
-    }
-    
-        
-}
->>>>>>> origin/master
