916ae93200c2fa45c3f759184de40a487e1c1b7f
JP <johnpaul.hernandez@selu.edu>
2015-10-27 11:49:55 -0700

    Money System

--- Sentinel of the Orchard/Assets/_Scripts/Money/MoneySystem.cs
+++ Sentinel of the Orchard/Assets/_Scripts/Money/MoneySystem.cs
@@ -2,18 +2,19 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class playerMoney : MonoBehaviour
+public class MoneySystem : MonoBehaviour
 {
     public int CurrentMoney;
-    public Text CurrentMoney_Counter;
+    public Text CurrentMoney_Text;
 
 
     // Use this for initialization
     void Start()
     {
+        //player's total money
         CurrentMoney = 0;
-        //No UI for this yet
-        CurrentMoney_Counter.text = CurrentMoney.ToString();
+        //presents money on the screen
+        CurrentMoney_Text.text = CurrentMoney.ToString();
     }
 
     // Update is called once per frame
@@ -24,9 +25,9 @@ public class playerMoney : MonoBehaviour
 
     public void GainMoney(int MoneyGained)
     {
-        //adds money to player total money and presents it in money UI
+        //adds money to player and presents it in money UI
         CurrentMoney += MoneyGained;
-        CurrentMoney_Counter.text = CurrentMoney.ToString();
+        CurrentMoney_Text.text = CurrentMoney.ToString();
 
     }
 
@@ -35,13 +36,14 @@ public class playerMoney : MonoBehaviour
 
         if (CurrentMoney - MoneyLost < 0)
         {
+            //showed in console when player doesnt have enough money to lose.
             Debug.Log("You require additional money.");
 
         }else
         {
             //subtracts money from player and presents in to money UI
             CurrentMoney -= MoneyLost;
-            CurrentMoney_Counter.text = CurrentMoney.ToString();
+            CurrentMoney_Text.text = CurrentMoney.ToString();
 
         }
     }
