1e10c8c1e18192cee53b30293af5b56e47fee720
Ravarenos <joshua.volkmann@selu.edu>
2015-11-13 22:53:57 -0600

    Options Menu
    Work in Progress

---
+++ Sentinel of the Orchard/Assets/_Scripts/Menu/ResToggle.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResToggle : MonoBehaviour
+{
+    bool isPressed = false;
+    public GameObject panel;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            togglePanel(isPressed, panel);
+        }
+    }
+
+    void togglePanel(bool isPressed, GameObject panel)
+    {
+        if(isPressed)
+        {
+            panel.SetActive(isPressed);
+            isPressed = !isPressed;
+        }
+        else
+        {
+            panel.SetActive(isPressed);
+            isPressed = !isPressed;
+        }
+    }
+}
